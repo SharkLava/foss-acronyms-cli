c100b4bc96408ec898abb9dfe8e93bbde763e6d4
Vishal Kalathil <kalathilvishal1@gmail.com>
2022-10-18 15:10:29 +0530

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
 use downloader::Downloader;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+fn typed_example() -> Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "Abbreviation": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into a Person object. This is exactly the
+    // same function as the one that produced serde_json::Value above, but
+    // now we are asking it for a Person as output.
+    let p: Person = serde_json::from_str(data)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("Please call {} at the number {}", p.name, p.phones[0]);
+
+    Ok(())
+}
 
 // Run example with: cargo run --example tui_basic --features tui
 fn download() {
@@ -27,4 +59,6 @@ fn main() {
     if !std::path::Path::new("data/acronyms.json").exists() {
         download();
     }
+    let res = typed_example();
+    print!("{:?}", res);
 }
